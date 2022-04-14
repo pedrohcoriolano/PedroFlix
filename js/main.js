dbab153aff2ea93616c89195c295ef920a9f1b52
Pedro Henrique Coriolano Oliveira Cruz <93086323+pedrohcoriolano@users.noreply.github.com>
2022-04-14 16:02:26 -0300

    arquivos

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function informar () {
+    alert('O congressista Francis Underwood e sua mulher, Claire, fazem de tudo para conquistar seus objetivos, não importa o que aconteça. Um mundo político recheado de ganância, corrupção e luxúria na capital Washington.')
+}
